17048823ef2fd95a6c85490f8799fc40edf47aba
st1llwater <alokpunjofficial@gmail.com>
2021-09-11 09:54:04 +0530

    Completed Functionality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import Board from "./Components/Board";
+import calculateWinner from "./helper";
 
 import "./Styles/root.scss";
 
 function App() {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(false);
+
+  const winner = calculateWinner(board);
+
+  const message = winner
+    ? `Winner Is  ${winner}`
+    : `Next Player To Mark: ${isXNext ? "X" : "O"}`;
+
+  const handleSquareClick = (position) => {
+    if (board[position] || winner) {
+      return;
+    }
+
+    setBoard((prev) => {
+      return prev.map((square, pos) => {
+        if (pos === position) {
+          return isXNext ? "X" : "O";
+        } else {
+          return square;
+        }
+      });
+    });
+    setIsXNext(!isXNext);
+  };
+
   return (
     <div className="app">
       <h1>Tic Tac Toe</h1>
-      <Board />
+      <h1>{message}</h1>
+      <Board board={board} handleSquareClick={handleSquareClick} />
     </div>
   );
 }
