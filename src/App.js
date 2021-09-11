323125366937623ee4ae06f46477d41ed497960b
st1llwater <alokpunjofficial@gmail.com>
2021-09-11 08:31:10 +0530

    Added Sass For Styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Board from "./Components/Board";
 
+import "./Styles/root.scss";
+
 function App() {
   return (
-    <>
-      <h1>Tic-Tac-Toe</h1>
+    <div className="app">
+      <h1>Tic Tac Toe</h1>
       <Board />
-    </>
+    </div>
   );
 }
 
